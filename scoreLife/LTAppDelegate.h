c8879826b6b5234a81183821e1c1011d278775c4
Tao Yunfei <traintrackcn@gmail.com>
2013-01-24 12:14:11 +0800

    init xcode prj

---
+++ scoreLife/LTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LTAppDelegate.h
+//  scoreLife
+//
+//  Created by traintrackcn on 13-1-24.
+//  Copyright (c) 2013年 lt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ scoreLife/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  scoreLife
+//
+//  Created by traintrackcn on 13-1-24.
+//  Copyright (c) 2013年 lt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LTAppDelegate class]));
+    }
+}
